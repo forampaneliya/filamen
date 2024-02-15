12bf3a65be9afdcc057c57da7ce5022a35b38e8f
forampaneliya <159258864+forampaneliya@users.noreply.github.com>
2024-02-15 16:36:30 +0530

    Add files via upload

---
+++ q-2.c
@@ -0,0 +1,33 @@
+//Q.2 Write a Program to count the frequency of each character in a given string.
+#include<stdio.h>
+//#include<string.h>
+
+ void main()
+{
+	char s[100];
+	int i,j,k,count=0,n;
+	printf("enter any string = ");
+	gets(s);
+	for(j=0;s[j]!=NULL;j++)
+	{
+		
+	}
+	n=j;
+	printf("frequncy count character in string =\n");
+	for(i=0;i<n;i++)
+	{
+		count=1;
+		if(s[i]!=NULL)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(s[i]==s[j])
+				{
+					count++;
+					s[j]='\0';
+				}
+			}
+			printf("%c =%d\n",s[i],count);
+		}
+	}
+}
